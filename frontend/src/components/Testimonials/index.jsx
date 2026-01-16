be60647eb7e7c1073e88e5d91cecef15c022741a
talhacodess <contact@talhacodes.com>
2026-01-16 20:47:22 +0500

    update review bg

--- frontend/src/components/Testimonials/index.jsx
+++ frontend/src/components/Testimonials/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import Button from "../common/Button";
 
+
 const Testimonials = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -14,23 +15,8 @@ const Testimonials = () => {
         const result = await response.json();
         const reviewsData = result?.reviews || result?.data?.reviews || result;
         
-<<<<<<< HEAD
         if (Array.isArray(reviewsData)) {
           setTestimonials(reviewsData);
-=======
-        if (result.status === "success" && result.data) {
-          // Map API data to component structure
-          const mappedTestimonials = result.data.reviews.map((item) => ({
-            id: item._id,
-            name: item.name,
-            location: formatDate(item.date),
-            rating: item.rating,
-            review: item.review,
-            email: item.email,
-            date: item.date,
-          }));
-          setTestimonials(mappedTestimonials);
->>>>>>> 621b377705e13a47afc6c85b7094d78e6345bb60
         }
       } catch (error) {
         console.error("Error fetching testimonials:", error);
@@ -42,11 +28,11 @@ const Testimonials = () => {
   }, []);
 
   if (loading || testimonials.length === 0) {
-    return <div className="py-20 text-center font-['Quicksand'] text-[#213554]">Loading Reviews...</div>;
+    return <div className="py-20 text-center  text-[#213554]">Loading Reviews...</div>;
   }
 
   return (
-    <div className="py-16 bg-[#fdfeff] font-['Quicksand']">
+    <div className="py-16  ">
       <section className="overflow-hidden relative">
         <style>
           {`
@@ -71,7 +57,7 @@ const Testimonials = () => {
 
         <div className="max-w-8xl mx-auto text-center px-6">
 
-          <div className="testimonial-mask pause-on-hover">
+          <div className="testimonial-mask pause-on-hover p-3">
             <div className="animate-marquee gap-6">
               {/* We duplicate the array to create the infinite loop effect */}
               {[...testimonials, ...testimonials].map((testimonial, index) => {
@@ -83,9 +69,9 @@ const Testimonials = () => {
                     key={index} 
                     className="p-8 bg-white border border-slate-100 w-[350px] md:w-[400px] rounded-[2rem] shadow-sm shrink-0 flex flex-col text-left whitespace-normal hover:shadow-md transition-shadow"
                   >
-                    <div className="flex items-center gap-4 mb-6">
+                    <div className="flex items-center gap-4 mb-6 ">
                       <div 
-                        className="w-12 h-12 rounded-full bg-[#ee334b] text-white flex items-center justify-center font-bold text-sm shrink-0 shadow-sm"
+                        className="w-12 h-12 rounded-full bg-[#ee334b] text-white flex items-center justify-center font-bold text-sm shrink-0 shadow-sm m-2"
                         style={{ 
                           backgroundImage: testimonial.profile_photo_url ? `url(${testimonial.profile_photo_url})` : 'none', 
                           backgroundSize: 'cover',
@@ -122,7 +108,7 @@ const Testimonials = () => {
           </div>
 
           <div className="mt-16">
-            <Button label={"View More Reviews on Google"}/>
+           <a href="https://www.google.com/search?sca_esv=1af006d14f18ca7f&rlz=1C1RXMK_en-GBPK1190PK1190&sxsrf=ANbL-n41RZb-0JiUnyL3pswQbyVTBtV2Kw:1768577776703&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOf1vgl_3Y0ETzIquowROB-BxkPZJM6EWcTFzWmcnsl-FZmExVeO-CVINtkvBL5KVhJ0BASn6vpMB-EjQ2gUvw7Wr3d4dHk1adWvBj2D6YmZXCO9pIw%3D%3D&q=Umbrella+Custom+Packaging+USA+Reviews&sa=X&ved=2ahUKEwjOkY3RsZCSAxXaVUEAHZasDSsQ0bkNegQIWBAH&biw=1680&bih=923&dpr=1&aic=0" target="_blank" rel="noopener noreferrer"> <Button label={"View More Reviews on Google"} /></a>
             </div>
         </div>
       </section>
