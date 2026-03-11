f4a2ffb9796604888c5fb0efac02cbd5671df295
Ghulam89 <gm6681328@gmail.com>
2026-03-12 00:34:26 +0500

    update

--- frontend/src/utils/BaseUrl.jsx
+++ frontend/src/utils/BaseUrl.jsx
@@ -1,4 +1,4 @@
 const SiteUrl = import.meta.env.VITE_SITE_BASE_URL || "https://xcustompackaging.com";
 export const BaseUrl = SiteUrl;
 export const ApiBaseUrl =
-  import.meta.env.VITE_API_BASE_URL || `${SiteUrl}`;
+  import.meta.env.VITE_API_BASE_URL || `${SiteUrl}` || "https://xcustompackaging.com";
